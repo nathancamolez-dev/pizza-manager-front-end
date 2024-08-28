376e5392ffe8a00d7745e0a26b2d0a04a05cfe27
nathancamolez-dev <nathan.camolez@unesp.br>
2024-08-28 03:08:16 -0300

    feat: Layouts for router and react-helmet

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
-  return <h1>Sing in</h1>
+  return (
+    <>
+      <Helmet title="Login" />
+      <h1>Sing in</h1>
+    </>
+  )
 }
