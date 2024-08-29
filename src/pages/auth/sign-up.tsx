4ff0c3426393c990c6de666550975bdb6960a7c5
nathancamolez-dev <nathan.camolez@unesp.br>
2024-08-29 16:15:13 -0300

    feat: login page and register page

---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,105 @@
+import { useEffect } from 'react'
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signUpForm = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
+  email: z.string().email(),
+})
+
+type SignUpForm = z.infer<typeof signUpForm>
+
+export function SignUp() {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isSubmitted },
+  } = useForm<SignUpForm>()
+
+  async function handleSignUp(data: SignUpForm) {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    toast.success('Restaurante cadastrado com sucesso!')
+  }
+
+  useEffect(() => {
+    if (isSubmitted) {
+      navigate('/sign-in')
+    }
+  }, [isSubmitted, navigate])
+
+  return (
+    <>
+      <Helmet title="Cadastro" />
+      <div className="p-8">
+        <Button variant={'ghost'} asChild className="absolute right-8 top-8">
+          <Link to="/sign-in">Login</Link>
+        </Button>
+
+        <div className="w-[350]px flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Criar conta grátis
+            </h1>
+            <p className="text-sm text-muted-foreground">Seja um parceiro!</p>
+          </div>
+
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Nome do estabelecimento</Label>
+              <Input
+                id="restaurantName"
+                type="text"
+                {...register('restaurantName')}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu nome</Label>
+              <Input
+                id="managerName"
+                type="text"
+                {...register('managerName')}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu telefone</Label>
+              <Input id="phone" type="tel" {...register('phone')} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu email</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+            <div className="flex justify-end ">
+              <Button disabled={isSubmitting} type="submit">
+                Finalizar
+              </Button>
+            </div>
+            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              Ao continuar, você concorda com os nossos
+              <a className="underline underline-offset-4" href="">
+                termos de serviço
+              </a>{' '}
+              e{' '}
+              <a className="underline underline-offset-4" href="">
+                políticas de privacidade
+              </a>
+              .
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
