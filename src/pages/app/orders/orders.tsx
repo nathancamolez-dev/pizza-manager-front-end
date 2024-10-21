53a626d06ba3b169e7f6733fd30a1a80c5b256d0
nathancamolez-dev <nathan.camolez@unesp.br>
2024-10-21 15:41:25 -0300

    feat: pagination for orders

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -14,6 +14,8 @@ import { OrderTableFilters } from './orders-table-filters'
 import { useQuery } from '@tanstack/react-query'
 
 import { getOrders } from '@/api/get-orders'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
 
 export interface OrderTableRowProps {
   orders: {
@@ -26,10 +28,25 @@ export interface OrderTableRowProps {
 }
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageIndex = z.coerce
+    .number()
+    .transform(page => page - 1)
+    .parse(searchParams.get('page') ?? '1')
+
   const { data: result } = useQuery({
-    queryKey: ['orders'],
-    queryFn: getOrders,
+    queryKey: ['orders', pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   })
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams(prev => {
+      prev.set('page', String(pageIndex + 1))
+      return prev
+    })
+  }
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -61,7 +78,14 @@ export function Orders() {
             </Table>
           </div>
 
-          <Pagination pageIndex={0} totalCount={100} perPage={10} />
+          {result && (
+            <Pagination
+              onPageChange={handlePaginate}
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+            />
+          )}
         </div>
       </div>
     </>
