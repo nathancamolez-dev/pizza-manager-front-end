a498beef72c1a6de784bc583390eeaf18d453a8d
nathancamolez-dev <nathan.camolez@unesp.br>
2024-10-28 15:51:21 -0300

    feat: Skeleton for loading state on dashboard card

--- src/pages/app/dashboard/month-orders-amount.tsx
+++ src/pages/app/dashboard/month-orders-amount.tsx
@@ -2,6 +2,7 @@ import { getMonthOrdersAmount } from '@/api/get-month-orders-amount'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { useQuery } from '@tanstack/react-query'
 import { Utensils } from 'lucide-react'
+import { MetricCardSkeleton } from './metric-card-skeleton'
 
 export function MonthOrdersAmountCard() {
   const { data: monthOrdersAmount } = useQuery({
@@ -17,7 +18,7 @@ export function MonthOrdersAmountCard() {
         <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {monthOrdersAmount && (
+        {monthOrdersAmount ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {monthOrdersAmount.orders.toLocaleString('pt-BR')}
@@ -42,6 +43,8 @@ export function MonthOrdersAmountCard() {
               )}
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
